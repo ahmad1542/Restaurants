28e8baa84fad13a2f082e8cd321b2f3b564fd9b4
Cyborg99 <ahmad.husien4@gmail.com>
2025-08-29 17:26:43 +0300

    Add project files.

---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,27 @@
+﻿using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Seeders {
+    internal class RestaurantSeeder(RestaurantsDbContext dbContext) {
+
+        public async Task Seed() {
+            if (await dbContext.Database.CanConnectAsync()) {
+                if (!dbContext.Restaurants.Any()) {
+                    List<Restaurant> restaurants = [
+                            new() {
+                                Name = "KFC",
+                                Category = "Fast Food",
+                                Description = "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquarter"
+                            }
+                        ]
+                }
+            }
+        }
+
+    }
+}
