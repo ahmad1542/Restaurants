4e59498abb781c40f748b2545558afdcd4cddb86
Cyborg99 <ahmad.husien4@gmail.com>
2025-09-02 18:39:49 +0300

    The RestaurantDto and DishDto classes were added, and the IRestaurantService interface along with its implementation were successfully modified.

---
+++ Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
@@ -0,0 +1,33 @@
+﻿using Restaurants.Application.Dishes.Dtos;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Restaurants.Dtos; 
+public class RestaurantDto {
+    public int Id { get; set; }
+    public string Name { get; set; } = "Ahmad Hussein";
+    public string Description { get; set; } = default!;
+    public string Category { get; set; } = default!;
+    public bool HasDelivery { get; set; }
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+    
+    public List<DishDto> Dishes { get; set; } = new();
+
+    public static RestaurantDto? FromEntity(Restaurant? restaurant) {
+
+        if (restaurant is null) return null;
+
+        return new RestaurantDto() {
+            Id = restaurant.Id,
+            Name = restaurant.Name,
+            Category = restaurant.Category,
+            Description = restaurant.Description,
+            HasDelivery = restaurant.HasDelivery,
+            City = restaurant.Address?.City,
+            Street = restaurant.Address?.Street,
+            PostalCode = restaurant.Address?.PostalCode,
+        };
+    }
+
+}
--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants;
 internal class RestaurantsService(IRestaurantRepository restaurantRepository, ILogger<RestaurantsService> logger) : IRestaurantsService {
 
-    public async Task<IEnumerable<Restaurant>> GetAllRestaurants() {
+    public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants() {
         logger.LogInformation("Getting All Restaurants");
         var restaurants = await restaurantRepository.GetAllAsync();
-        return restaurants;
+
+        var restaurantsDtos = restaurants.Select(RestaurantDto.FromEntity);
+        return restaurantsDtos!;
     }
 
-    public async Task<Restaurant?> GetById(int id) {
+    public async Task<RestaurantDto?> GetById(int id) {
         logger.LogInformation($"Getting The Restaurant With Id {id}");
         var restaurant = await restaurantRepository.GetByIdAsync(id);
-        return restaurant;
+        var restaurantDto = RestaurantDto.FromEntity(restaurant);
+
+        return restaurantDto;
     }
 }
